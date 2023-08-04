1b41d9d64958dacf16391979d56fbb9e1abb3876
nox1d <114980767+nox1d@users.noreply.github.com>
2023-08-04 16:24:47 +0800

    Pass test1 in 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringInput = '';
+    for(let i = 0; i < num; i++) {
+        stringInput += string;
+    }
+    return stringInput;
 };
 
 // Do not edit below this line
