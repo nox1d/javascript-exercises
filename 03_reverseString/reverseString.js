9e42a7c50a30f02bd0ce5042f062359bbc5fe70c
nox1d <114980767+nox1d@users.noreply.github.com>
2023-08-04 17:01:16 +0800

    Pass test1 in 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const char = string.split('');
+    let newString = [];
+    for (let i = char.length - 1; i >= 0; i--) {
+        newString.push(char[i]);
+    }
+    
+    return newString.join('');
 };
 
 // Do not edit below this line
