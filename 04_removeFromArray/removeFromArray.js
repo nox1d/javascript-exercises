e64cf951466be5a158c6a26b6123a60b57070c59
nox1d <114980767+nox1d@users.noreply.github.com>
2023-08-06 17:11:35 +0800

    Pass test1 in 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    // Find the index of theArgs inside the array
+    // Slice the element
+    let elementToRemove;
+    for(const arg of theArgs) {
+        elementToRemove = array.findIndex((x) => x === arg);
+        array.splice(elementToRemove, 1);
+    }
+    return array;
 
 };
 
