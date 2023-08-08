3eae45b48bfbe782de545a9134ace33fc116847d
nox1d <114980767+nox1d@users.noreply.github.com>
2023-08-08 08:54:29 +0800

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,14 @@
 const sumAll = function(start, end) {
     let result = 0;
-    if (start < end) {
+    
+    if (!(Number.isInteger(start)) || !(Number.isInteger(end)) || start < 0 || end < 0) {
+        return 'ERROR';
+    } else if (start < end) {
         for (let i = start; i <= end; i++) {
             result += i;
         }
     } else {
         for (let i = start; i >= end; i--) {
-            console.log(i);
             result += i;
         }
     };
