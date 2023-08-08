cb5dbb5c8167ec5e6a82a658d5216f03508b1261
nox1d <114980767+nox1d@users.noreply.github.com>
2023-08-08 09:12:53 +0800

    Finish 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(year) {
-    if (year % 100 === 0 && year % 400 !== 0 && year % 4 !== 0) return false;
-    return true;
-
+    if (year % 100 === 0 && year % 400 !== 0) return false;
+    if (year % 4 === 0) return true;
+    return false;
 };
 
 // Do not edit below this line
