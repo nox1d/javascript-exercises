8b8429914b4d5af2c83fbf9dab850774b755e2dc
nox1d <114980767+nox1d@users.noreply.github.com>
2023-08-08 09:32:02 +0800

    Pass test 1 in 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  const conversion = (degree - 32) * 5 / 9; 
+  if (!Number.isInteger(conversion)) {
+    return parseFloat(conversion).toFixed(1);
+  }
+  return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  const conversion = (degree * 5 / 9) + 32;
+  if (!Number.isInteger(conversion)) {
+    return parseFloat(conversion).toFixed(1);
+  }
+  return conversion;
 };
 
 // Do not edit below this line
