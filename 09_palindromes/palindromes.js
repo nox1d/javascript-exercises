260b3f90bed6d47a65fcde73b7e730b4975cb05f
nox1d <114980767+nox1d@users.noreply.github.com>
2023-09-06 16:38:44 +0800

    Pass test 2, 3 palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
 const palindromes = function (str) {
+    str.toLowerCase();
+    if (str.includes('!')) {
+        str.split().splice('!');
+    }
     return (str === str.split().reverse().join(''));
 
 };
